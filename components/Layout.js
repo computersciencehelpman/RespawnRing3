2a1c5a7d7b20aabbfdc0756e6ddc0e94e25225a2
Saurav Bhatt <55709609+computersciencehelpman@users.noreply.github.com>
2025-06-24 15:34:13 -0400

    Froth key page added

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Rightbar from './Rightbar';
@@ -33,27 +34,28 @@ export default function Layout({ children }) {
 
       {/* ✅ Froth Key image - truly fixed bottom-right */}
       {isLeaderboard && (
-        <div
-          className="z-[1000]"
-          style={{
-            position: 'fixed',
-            bottom: '1rem',
-            right: '1rem',
-            pointerEvents: 'none',
-          }}
-        >
-          <div
-            className="bg-black/80 p-2 rounded-lg shadow-lg backdrop-blur-md"
-            style={{ width: '14rem', maxHeight: '200px' }}
-          >
-            <img
-              src="/images/Froth.png"
-              alt="Froth Key"
-              className="w-full h-auto object-contain rounded opacity-90"
-            />
-          </div>
-        </div>
-      )}
+  <div
+    className="z-[1000]"
+    style={{
+      position: 'fixed',
+      bottom: '1rem',
+      right: '1rem',
+    }}
+  >
+    <Link href="/froth">
+      <div
+        className="bg-black/80 p-2 rounded-lg shadow-lg backdrop-blur-md cursor-pointer"
+        style={{ width: '14rem', maxHeight: '200px' }}
+      >
+        <img
+          src="/images/Froth.png"
+          alt="Froth Key"
+          className="w-full h-auto object-contain rounded opacity-90"
+        />
+      </div>
+    </Link>
+  </div>
+)}
     </div>
   );
 }
